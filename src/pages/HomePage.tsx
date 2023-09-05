f50f4978668d3f9b15861596ff08472a4390ee25
Evan <evan.epifanius@gmail.com>
2023-09-05 22:55:35 +0700

    update layout pokemon home

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,14 +3,25 @@ import FilterPokemon from "../components/FilterPokemon";
 import PokemonList from "../components/PokemonList";
 
 export default function HomePage() {
+  const [filterType, setFilterType] = useState<string>("");
 
-  const [filterType,setFilterType]=useState<string>("")
-  
   return (
     <>
-      <h1 className="text-4xl text-white mb-2 font-bold">Pokedex</h1>
-      <FilterPokemon setFilterType={setFilterType} filterType={filterType} />
-      <PokemonList type={filterType} />
+      <div className="fieldbg">
+        <img
+          src="/landscape.jpg"
+          className=" w-full h-full z-[-1] top-0 left-0  absolute"
+          alt=""
+        />
+        <div className="container  p-5 mx-auto flex flex-col h-full">
+          <h1 className="text-4xl text-white mb-2 font-bold">Pokedex</h1>
+          <FilterPokemon
+            setFilterType={setFilterType}
+            filterType={filterType}
+          />
+          <PokemonList type={filterType} />
+        </div>
+      </div>
     </>
   );
 }
